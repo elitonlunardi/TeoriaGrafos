179adf697fcd7f7bc9c8bb2f05c7dacbd736f4bb
Éliton Lunardi <lunardi.eliton@gmail.com>
2020-09-27 19:22:47 -0300

    code smells

--- src/ComparadorInt.java
+++ src/ComparadorInt.java
@@ -1,10 +1,10 @@
+//Éliton Lunardi
 import java.util.Comparator;
 
 public class ComparadorInt implements Comparator<Integer>{
 
 	@Override
 	public int compare(Integer o1, Integer o2) {
-		// TODO Auto-generated method stub
 		if (o1 < o2) {
 			return -1;
 		}
